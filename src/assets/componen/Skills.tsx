730b2cd4d7069c5c199f77dd78383c19f04d6be2
K-ongpop Nualsaard <kongpopku@ku.th>
2024-01-26 18:20:04 +0700

    updete-project

--- src/assets/componen/Skills.tsx
+++ src/assets/componen/Skills.tsx
@@ -1,5 +1,5 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { useLayoutEffect } from "react";
 import { NavLink } from "react-router-dom";
 const Skills = () => {
